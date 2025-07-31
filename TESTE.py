ef3a19b90d3268b4c425f4473b2d0e0a0efc6ebb
Gabriel Sousa Guimarães <79774448+Gabrielp-sg@users.noreply.github.com>
2025-07-31 17:05:33 -0300

    Update TESTE.py

--- TESTE.py
+++ TESTE.py
@@ -1,1 +1,48 @@
 
+Job ID
+995705
+Status
+Successful
+Started
+31/07/2025, 16:30:44
+Finished
+31/07/2025, 16:38:06
+Job Template
+0072-wkl-d-lpbr-apps-lpfat-linux
+Job Type
+Playbook Run
+Launched By
+0072-wkl-d-automation-user
+Inventory
+0072-wkl-d-lpbr-apps-lpfat
+Project
+0072-wkl-lpbr-apps-lpfat
+Project Update Status
+Successful
+Source Control Branch
+WLDOPT-2515-lpfat-development
+Revision
+bdda9b9dd97a6998821c1189c81c5617183f8d1d
+Playbook
+callback.yml
+Verbosity
+0 (Normal)
+Execution Environment
+LeasePlan LZ Default Execution Environment (ansible core v2.12)
+Controller Node
+awx-v2-7b4d87d6cb-9wqlw
+Container Group
+default
+Job Slice
+0/1
+Forks
+0
+Timeout
+No timeout specified
+Credentials
+Cloud: 0072-d-aws-ssm
+Cloud: 0072-d-vault-token
+Created
+31/07/2025, 16:30:16 by 0072-wkl-d-automation-user
+Last Modified
+31/07/2025, 16:30:44
