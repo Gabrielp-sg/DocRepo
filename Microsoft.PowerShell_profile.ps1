07b613eec79918d8445ed5c6cb767bac88ce00c5
Gabriel Sousa Guimarães <79774448+Gabrielp-sg@users.noreply.github.com>
2025-08-07 15:14:16 -0300

    Update Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -12,3 +12,6 @@ C:\STCPCLT_BRADESCO\O0055BRADESCO\ENTRADA
 C:\STCPCLT_BRADESCO\O0055BRADESCO\SAIDA
 
 O EBS é o que anexado a EC2 windows do LpFAT.
+
+podemos já ir para o próximo issue do lpfat que é expor dois diretórios de um ebs para a ec2 onde ta rodando a aplicação
+acho que tem um card lá disso, se não tiver eu vou criar com as pastas 
