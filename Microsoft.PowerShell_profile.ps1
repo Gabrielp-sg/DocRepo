2a7afdaeab5bd937e5cb45648824d15a21fd13ae
Gabriel Sousa Guimarães <79774448+Gabrielp-sg@users.noreply.github.com>
2025-08-11 16:39:41 -0300

    Update Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,147 +1,91 @@
-{
-  "changed": false,
-  "msg": "account_name AM\\guimd is not a valid account, cannot get SID: Exception calling \"Translate\" with \"1\" argument(s): \"Some or all identity references could not be translated.\"",
-  "_ansible_no_log": false
+# Alias configuration
+Set-Alias -Name k -Value "C:\Program Files\Docker\Docker\resources\bin\kubectl.exe"
+# $vaultExe = "C:\Program Files\HashiCorp\Vault\vault.ext"
+# Custom environment configuration
+$env:AWS_DEFAULT_REGION = 'sa-east-1'
+$proxy='http://LPGPZENPROXY.EMEA.LEASEPLANCORP.NET:80'
+$ENV:HTTP_PROXY=$proxy
+$ENV:HTTPS_PROXY=$proxy
+$ENV:NO_PROXY='localhost,127.0.0.0/8,10.0.0.0/8,172.16.0.0/12,192.168.0.0/16,core-services.leaseplan.systems'
+
+# Set options for PSReadLine using Fzf, if Fzf is installed and configured
+if (Get-Module -ListAvailable -Name PSFzf) {
+    Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+t' -PSReadlineChordReverseHistory 'Ctrl+r'
 }
 
-
-{
-  "module_stdout": "{\"changed\":false,\"invocation\":{\"module_args\":{\"_measure_subset\":false,\"gather_timeout\":10,\"gather_subset\":[\"all\"],\"fact_path\":null}},\"ansible_facts\":{\"ansible_ip_addresses\":[\"10.132.5.9\"],\"ansible_windows_domain_role\":\"Member server\",\"ansible_architecture2\":\"x86_64\",\"ansible_user_gecos\":\"\",\"ansible_distribution_major_version\":\"10\",\"ansible_pagefiletotal_mb\":1147,\"ansible_os_name\":\"Microsoft Windows Server 2022 Datacenter\",\"ansible_system_description\":\"\",\"ansible_machine_id\":\"S-1-5-21-3365362568-487274154-3504603634\",\"ansible_product_serial\":\"ec211e3c-6b28-51af-6b84-487bfb8feeae\",\"ansible_system_vendor\":\"Amazon EC2\",\"gather_subset\":[\"all\"],\"ansible_bios_version\":\"1.0\",\"ansible_user_id\":\"LPLPFATWIND0072$\",\"ansible_date_time\":{\"epoch_local\":\"1754923097.67581\",\"epoch_int\":1754923098,\"date\":\"2025-08-11\",\"second\":\"17\",\"tz\":\"UTC\",\"iso8601_micro\":\"2025-08-11T14:38:17.675808Z\",\"iso8601_basic_short\":\"20250811T143817\",\"minute\":\"38\",\"day\":\"11\",\"weekday\":\"Monday\",\"iso8601\":\"2025-08-11T14:38:17Z\",\"tz_offset\":\"+00:00\",\"iso8601_basic\":\"20250811T143817675808\",\"epoch\":\"1754923097.67581\",\"weekday_number\":\"1\",\"hour\":\"14\",\"year\":\"2025\",\"month\":\"08\",\"time\":\"14:38:17\",\"weeknumber\":\"31\"},\"ansible_user_dir\":\"C:\\\\Windows\\\\system32\\\\config\\\\systemprofile\",\"ansible_processor_vcpus\":2,\"ansible_user_sid\":\"S-1-5-21-3365362568-487274154-3504603634-1000\",\"ansible_owner_contact\":\"\",\"ansible_distribution\":\"Micro\rsoft Windows Server 2022 Datacenter\",\"ansible_processor\":[\"0\",\"GenuineIntel\",\"Intel(R) Xeon(R) Platinum 8259CL CPU @ 2.50GHz\",\"1\",\"GenuineIntel\",\"Intel(R) Xeon(R) Platinum 8259CL CPU @ 2.50GHz\"],\"ansible_powershell_version\":5,\"ansible_uptime_seconds\":1185201,\"module_setup\":true,\"ansible_interfaces\":[{\"dns_domain\":null,\"connection_name\":\"Ethernet 3\",\"default_gateway\":\"10.132.4.1\",\"mtu\":1500,\"interface_name\":\"Amazon Elastic Network Adapter\",\"ipv6\":{},\"macaddress\":\"02:5A:69:76:03:FD\",\"speed\":4999.999,\"interface_index\":8,\"ipv4\":{\"address\":\"10.132.5.9\",\"prefix\":\"23\"}}],\"ansible_distribution_version\":\"10.0.20348.0\",\"ansible_windows_domain_member\":true,\"ansible_swaptotal_mb\":0,\"ansible_hostname\":\"LPLPFATWIND0072\",\"ansible_virtualization_role\":\"guest\",\"ansible_memfree_mb\":676,\"ansible_processor_count\":1,\"ansible_os_product_type\":\"server\",\"ansible_netbios_name\":\"LPLPFATWIND0072\",\"ansible_product_name\":\"t3.small\",\"ansible_virtualization_type\":\"kvm\",\"ansible_os_installation_type\":\"Server\",\"ansible_lastboot\":\"2025-07-28 21:24:57Z\",\"ansible_architecture\":\"64-bit\",\"ansible_bios_date\":\"10/16/2017\",\"ansible_processor_cores\":1,\"ansible_fqdn\":\"LPLPFATWIND0072.leaseplancorp.net\",\"ansible_domain\":\"leaseplancorp.net\",\"ansible_memtotal_mb\":2010,\"ansible_kernel\":\"10.0.20348.0\",\"ansible_pagefilefree_mb\":405,\"ansible_nodename\":\"LPLPFATWIND0072.leaseplancorp.net\",\"ansible_os_family\":\"Windows\",\"ansible_processor_threads_per_core\":2,\"ansible_owner_name\":\"EC2\",\"ansible_env\":{\"PROCESSOR_IDENTIFIER\":\"Intel64 Family 6 Model 85 Stepping 7, GenuineIntel\",\"ComSpec\":\"C:\\\\Windows\\\\system32\\\\cmd.exe\",\"DriverData\":\"C:\\\\Windows\\\\System32\\\\Drivers\\\\DriverData\",\"LOCALAPPDATA\":\"C:\\\\Windows\\\\system32\\\\config\\\\systemprofile\\\\AppData\\\\Local\",\"Path\":\"C:\\\\Windows\\\\system32;C:\\\\Windows;C:\\\\Windows\\\\System32\\\\Wbem;C:\\\\Windows\\\\System32\\\\WindowsPowerShell\\\\v1.0\\\\;C:\\\\Windows\\\\System32\\\\OpenSSH\\\\;C:\\\\Program Files\\\\Amazon\\\\cfn-bootstrap\\\\;C:\\\\Program Files\\\\Amazon\\\\AWSCLIV2\\\\;C:\\\\Windows\\\\system32\\\\config\\\\systemprofile\\\\AppData\\\\Local\\\\Microsoft\\\\WindowsApps\",\"SystemRoot\":\"C:\\\\Windows\",\"ALLUSERSPROFILE\":\"C:\\\\ProgramData\",\"CommonProgramFiles(x86)\":\"C:\\\\Program Files (x86)\\\\Common Files\",\"ProgramW6432\":\"C:\\\\Program Files\",\"USERPROFILE\":\"C:\\\\Windows\\\\system32\\\\config\\\\systemprofile\",\"ProgramFiles(x86)\":\"C:\\\\Program Files (x86)\",\"PSModulePath\":\"C:\\\\Windows\\\\system32\\\\WindowsPowerShell\\\\v1.0\\\\Modules;C:\\\\ProgramFiles\\\\WindowsPowerShell\\\\Modules\",\"PSExecutionPolicyPreference\":\"Unrestricted\",\"USERDOMAIN\":\"LEASEPLANCORP\",\"SystemDrive\":\"C:\",\"EC2LAUNCH_TELEMETRY\":\"1\",\"OS\":\"Windows_NT\",\"CommonProgramFiles\":\"C:\\\\Program Files\\\\Common Files\",\"ProgramFiles\":\"C:\\\\Program Files\",\"USERNAME\":\"LPLPFATWIND0072$\",\"PROCESSOR_LEVEL\":\"6\",\"COMPUTERNAME\":\"LPLPFATWIND0072\",\"AWS_EXECUTION_ENV\":\"EC2\",\"NUMBER_OF_PROCESSORS\":\"2\",\"PUBLIC\":\"C:\\\\Users\\\\Public\",\"PROCESSOR_REVISION\":\"5507\",\"APPDATA\":\"C:\\\\Windows\\\\system32\\\\config\\\\systemprofile\\\\AppData\\\\Roaming\",\"ProgramData\":\"C:\\\\ProgramData\",\"PROCESSOR_ARCHITECTURE\":\"AMD64\",\"CommonProgramW6432\":\"C:\\\\Program Files\\\\Common Files\",\"windir\":\"C:\\\\Windows\",\"PATHEXT\":\".COM;.EXE;.BAT;.CMD;.VBS;.VBE;.JS;.JSE;.WSF;.WSH;.MSC;.CPL\",\"TEMP\":\"C:\\\\Windows\\\\TEMP\",\"TMP\":\"C:\\\\Windows\\\\TEMP\"},\"ansible_windows_domain\":\"leaseplancorp.net\",\"ansible_reboot_pending\":true,\"ansible_system\":\"Win32NT\"}}",
-  "module_stderr": "",
-  "msg": "MODULE FAILURE\nSee stdout/stderr for the exact error",
-  "rc": 0,
-  "_ansible_no_log": false,
-  "changed": false
+# Function to log into Vault using the Okta authentication method
+function vl { 
+    $username = "gabriel.guimaraes-ext@leaseplan.com"
+    vault login --method=okta username=$username
 }
 
-JT=4373
-ADMIN_ROLE_ID=$(curl -s -H "Authorization: Bearer $TOKEN" \
-  "$AWX_HOST/api/v2/job_templates/$JT/" \
-| jq -r '.summary_fields.object_roles.admin_role.id')
-echo "ADMIN_ROLE_ID=$ADMIN_ROLE_ID"
+# Function to open a Git Bash shell
+function Open-GitBash { 
+    & 'C:\Program Files\Git\bin\sh.exe' --login 
+}
 
-curl -s -H "Authorization: Bearer $TOKEN" \
-  "$AWX_HOST/api/v2/roles/$ADMIN_ROLE_ID/teams/?page_size=200" \
-| jq '.results[] | {id, name}'
+# Function to read AWS credentials from Vault and update AWS configuration
+function aws-vault-read {
+    param(
+        [Parameter(Mandatory)]
+        [string]$workload,
+        [string]$profileName = "default"  # Default profile name set to 'default'
+    )
+    Write-Output "Reading AWS credentials for workload: $workload"
 
-TEAM_ID=<ID_DO_TIME>
-curl -s -X POST -H "Authorization: Bearer $TOKEN" -H "Content-Type: application/json" \
-  -d '{"id": '"$TEAM_ID"', "disassociate": true}' \
-  "$AWX_HOST/api/v2/roles/$ADMIN_ROLE_ID/teams/"
-# Pegar IDs dos outros papéis do JT
-EXEC_ROLE_ID=$(curl -s -H "Authorization: Bearer $TOKEN" \
-  "$AWX_HOST/api/v2/job_templates/$JT/" \
-| jq -r '.summary_fields.object_roles.execute_role.id')
+    # Fetch AWS credentials from Vault
+    $awsCredentials = vault read "aws/$workload/creds/infra-userland" -format=json | ConvertFrom-Json
+    Write-Output "AWS Credentials fetched: Access Key = $($awsCredentials.data.access_key)"
 
-READ_ROLE_ID=$(curl -s -H "Authorization: Bearer $TOKEN" \
-  "$AWX_HOST/api/v2/job_templates/$JT/" \
-| jq -r '.summary_fields.object_roles.read_role.id')
+    # Update AWS CLI configuration with fetched credentials
+    aws configure set profile.$profileName.aws_access_key_id $awsCredentials.data.access_key
+    aws configure set profile.$profileName.aws_secret_access_key $awsCredentials.data.secret_key
+    aws configure set profile.$profileName.region 'eu-west-1'  # Set default region
 
-# Ver times com Execute e Read
-curl -s -H "Authorization: Bearer $TOKEN" "$AWX_HOST/api/v2/roles/$EXEC_ROLE_ID/teams/?page_size=200" \
-| jq '.results[] | {id,name}'
+    # Optionally, set this profile as default if required
+    aws configure set default.aws_access_key_id $awsCredentials.data.access_key
+    aws configure set default.aws_secret_access_key $awsCredentials.data.secret_key
+    aws configure set default.region 'eu-west-1'
 
-curl -s -H "Authorization: Bearer $TOKEN" "$AWX_HOST/api/v2/roles/$READ_ROLE_ID/teams/?page_size=200" \
-| jq '.results[] | {id,name}'
+    # Display the updated configuration for verification
+    aws configure list --profile $profileName
 
-# (Se houver) desassociar também
-TEAM_ID=<ID_DO_TIME>
-for RID in "$EXEC_ROLE_ID" "$READ_ROLE_ID"; do
-  curl -s -X POST -H "Authorization: Bearer $TOKEN" -H "Content-Type: application/json" \
-    -d '{"id": '"$TEAM_ID"', "disassociate": true}' \
-    "$AWX_HOST/api/v2/roles/$RID/teams/"
-done
+    # Setting AWS credentials in the current PowerShell session
+    $env:AWS_ACCESS_KEY_ID = $awsCredentials.data.access_key
+    $env:AWS_SECRET_ACCESS_KEY = $awsCredentials.data.secret_key
+    $env:AWS_DEFAULT_REGION = 'eu-west-1'
 
-AUTOMATION_USER="0072-wkl-d-automation-user"
-AUTOMATION_UID=$(curl -s -H "Authorization: Bearer $TOKEN" \
-  "$AWX_HOST/api/v2/users/?username=$AUTOMATION_USER" \
-| jq -r '.results[0].id')
+    if ($awsCredentials.data.PSObject.Properties.Name -contains 'security_token') {
+        $env:AWS_SESSION_TOKEN = $awsCredentials.data.security_token
+    }
 
-# Ver times do usuário
-curl -s -H "Authorization: Bearer $TOKEN" \
-  "$AWX_HOST/api/v2/users/$AUTOMATION_UID/teams/?page_size=200" \
-| jq '.results[] | {id,name}'
+    Write-Host "AWS Credentials updated for session: Access Key = $($awsCredentials.data.access_key)"
+}
 
-# Remover do time
-TEAM_ID=<ID_DO_TIME>
-curl -s -X POST -H "Authorization: Bearer $TOKEN" -H "Content-Type: application/json" \
-  -d '{"id": '"$AUTOMATION_UID"', "disassociate": true}' \
-  "$AWX_HOST/api/v2/teams/$TEAM_ID/users/"
+# Function to interactively select a workload and read credentials
+function usercred() { 
+    # List vault secrets and use Fzf to select one interactively
+    $workload = vault secrets list -format table | Select-String '[A-Z]{3,3}/\d{4}-[A-Z]{3,3}-[dtapm]/' | Invoke-Fzf 
+    $workload_env = ($workload.split(" ")[0]).split("/")[1]
 
-curl -s -H "Authorization: Bearer $TOKEN" \
-  "$AWX_HOST/api/v2/job_templates/$JT/access_list/?page_size=200" \
-| jq '.results[] | {username, role: .summary_fields.role_name}'
+    # Read and set AWS credentials for the selected workload
+    aws-vault-read $workload_env 
+    #lzt creds aws --role infra-userland --vault-addr https://vault.core-services.leaseplan.systems -S
+} 
 
+# Setting PowerShell execution policy for the current user if not already set
+if ((Get-ExecutionPolicy -Scope CurrentUser) -ne 'RemoteSigned') {
+    Set-ExecutionPolicy RemoteSigned -Scope CurrentUser -force
+    Set-ExecutionPolicy RemoteSigned -Scope LocalMachine -force
+    Set-ExecutionPolicy RemoteSigned -Scope Process -force
+}
 
-curl -s -H "Authorization: Bearer $TOKEN" \
-  "$AWX_HOST/api/v2/roles/$ADMIN_ROLE_ID/teams/?page_size=200" \
-| jq '.results'
+# Ensure the Vault address is set for the session
+$env:VAULT_ADDR = 'https://vault.core-services.leaseplan.systems'
+Write-Host "Vault Address is set to: $env:VAULT_ADDR"
 
+# Automatically update credentials on session start
+$initialWorkload = Read-Host "Please enter the initial workload (e.g., 9998-wkl-d)"
+aws-vault-read -workload $initialWorkload
 
-
-
-
-
-
-
-AUTOMATION_USER=0072-wkl-d-automation-user
-AUTOMATION_UID=$(curl -s -H "Authorization: Bearer $TOKEN" \
-  "$AWX_HOST/api/v2/users/?username=$AUTOMATION_USER" \
-| jq -r '.results[0].id')
-echo "$AUTOMATION_UID"
-
-curl -s -H "Authorization: Bearer $TOKEN" \
-  "$AWX_HOST/api/v2/users/$AUTOMATION_UID/tokens/?page_size=200&order_by=-modified" \
-| jq '.results[] | {id, description, scope, created, modified, summary_fields}'
-
-
-curl -s -H "Authorization: Bearer $TOKEN" \
-  "$AWX_HOST/api/v2/tokens/?page_size=200&order_by=-modified" \
-| jq '.results[] | select(.summary_fields.user.username=="'"$AUTOMATION_USER"'") \
-     | {id, description, scope, created, modified, summary_fields}'
-
-curl -s -H "Authorization: Bearer $TOKEN" \
-  "$AWX_HOST/api/v2/applications/?page_size=200" \
-| jq '.results[] | {id, name, client_type, authorization_grant_type, organization, summary_fields}'
-
-JT=4373
-EXEC_ROLE_ID=$(curl -s -H "Authorization: Bearer $TOKEN" \
-  "$AWX_HOST/api/v2/job_templates/$JT/" | jq -r '.summary_fields.object_roles.execute_role.id')
-
-# Times com Execute
-curl -s -H "Authorization: Bearer $TOKEN" \
-  "$AWX_HOST/api/v2/roles/$EXEC_ROLE_ID/teams/?page_size=200" \
-| jq '.results[] | {id,name}'
-
-# Usuários com Execute (confirma se o automation-user aparece)
-curl -s -H "Authorization: Bearer $TOKEN" \
-  "$AWX_HOST/api/v2/roles/$EXEC_ROLE_ID/users/?page_size=200" \
-| jq '.results[] | {id, username, first_name, last_name}'
-
-curl -s -H "Authorization: Bearer $TOKEN" \
-  "$AWX_HOST/api/v2/activity_stream/?actor=$AUTOMATION_UID&operation=create&object1=job&order_by=-timestamp&page_size=50" \
-| jq '.results[] | {timestamp, actor: .summary_fields.actor.username, operation, object1, changes:{id,name,job_template,extra_vars}}'
-
-
-# Desassociar usuário do papel Execute no JT
-curl -s -X POST -H "Authorization: Bearer $TOKEN" -H "Content-Type: application/json" \
-  -d '{"id": '"$AUTOMATION_UID"', "disassociate": true}' \
-  "$AWX_HOST/api/v2/roles/$EXEC_ROLE_ID/users/"
-
-for TID in $(curl -s -H "Authorization: Bearer $TOKEN" \
-  "$AWX_HOST/api/v2/users/$AUTOMATION_UID/tokens/?page_size=200" \
-| jq -r '.results[].id'); do
-  curl -s -X DELETE -H "Authorization: Bearer $TOKEN" "$AWX_HOST/api/v2/tokens/$TID/"
-done
-
-curl -s -X PATCH -H "Authorization: Bearer $TOKEN" -H "Content-Type: application/json" \
-  -d '{"is_active": false}' \
-  "$AWX_HOST/api/v2/users/$AUTOMATION_UID/"
-
-
-curl -s -H "Authorization: Bearer $TOKEN" \
-  "$AWX_HOST/api/v2/job_templates/$JT/jobs/?page_size=5&order_by=-created" \
-| jq '.results[] | {id, created, launch_type, launched_by: .summary_fields.created_by?.username}'
-
-
-
+Import-Module posh-git
