8ec97b59cb40829d757f69bba89e8a4cdd84cef0
Gabriel Sousa Guimarães <79774448+Gabrielp-sg@users.noreply.github.com>
2025-08-13 11:51:18 -0300

    Add files via upload

---
+++ development/providers.tf
@@ -0,0 +1,28 @@
+provider "aws" {
+  region = var.aws_region
+  default_tags {
+    tags = local.tags
+  }
+}
+
+provider "aws" {
+  alias  = "route53-zone-internal"
+  region = data.aws_region.current.name
+  assume_role {
+    role_arn = var.context == "pipeline" ? data.consul_keys.zone_internal.var.pipeline : data.consul_keys.zone_internal.var.userland
+  }
+}
+
+provider "consul" {
+  address    = "consul.core-services.leaseplan.systems"
+  datacenter = "euw1"
+  scheme     = "https"
+}
+
+provider "vault" {
+}
+
+provider "datadog" {
+  api_url = "https://api.datadoghq.eu/"
+}
+
---
+++ development/s3.tf
@@ -0,0 +1,15 @@
+module "aws_s3_lpfat" {
+  source = "git@gitlab.core-services.leaseplan.systems:shared/terraform_modules/aws/aws-s3-bucket.git?ref=v5.0.0"
+
+  name_prefix = format("lpfat-%s", module.shared_data.workload.environment_identifier)
+  versioning  = true
+  tags        = local.tags
+
+  lifecycle_rule = [{
+    id     = "expire_old_versions"
+    status = "Enabled"
+    noncurrent_version_expiration = {
+      noncurrent_days = 10
+    }
+  }]
+}
