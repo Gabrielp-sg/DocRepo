2b2ff41de8c462f1f563e15f06450edd5655076a
Gabriel Sousa Guimarães <79774448+Gabrielp-sg@users.noreply.github.com>
2025-08-22 18:16:46 -0300

    Update TESTE.py

--- TESTE.py
+++ TESTE.py
@@ -1,152 +1,382 @@
-COPY --from=cr.core-services.leaseplan.systems/hub.docker.com/maven:3.9.4-eclipse-temurin-11 ${MAVEN_HOME} ${MAVEN_HOME}
-
-include:
-  - project: 'templates/gitlab-ci/pipelines'
-    ref: old-pipeline
-    file: 'kaniko/login.yml'
-
-workflow:
-  rules:
-    - when: always
-
-stages:
-  - build
-  - development
-  - uat
-  - production
-
-variables:
-  LZ_KANIKO_EXECUTOR_IMAGE: leaseplan.jfrog.io/art-0001-docker-virtual/lz-kaniko:master
-  LZ_DOCKER_REGISTRY: https://leaseplan.jfrog.io
-  VAULT_AUTH_ROLE: workloads-0072-wkl-lpbr-apps
-  ARTIFACTORY_ROLE_DOCKER: art-0072-write-docker-local-default
-  KANIKO_EXTRA_ARGS: ""
-  KANIKO_EXTRA_ARGS_BUILD: ""
-  DOCKER_FILE: cadastro-positivo/cadastro-positivo-diretorio/Dockerfile
-  DOCKER_REGISTRY: leaseplan.jfrog.io/prv-0072-docker-local-default
-  DOCKER_IMAGE_NAME: cadastro-positivo-diretorio
-  VERSION: 1.0.0
-  container: docker
-
-build:
-  stage: build
-  image:  $LZ_KANIKO_EXECUTOR_IMAGE
-  extends:
-    - .kaniko-login
-  script:
-    - |
-      /kaniko/executor \
-        --context $CI_PROJECT_DIR \
-        --dockerfile $CI_PROJECT_DIR/$DOCKER_FILE \
-        --no-push
-
-development:
-  stage: development
-  dependencies:
-    - build
-  image:  $LZ_KANIKO_EXECUTOR_IMAGE
-  extends:
-    - .kaniko-login
-  script:
-    - echo "Deploying to development environment"
-    - |
-      /kaniko/executor \
-        --context $CI_PROJECT_DIR \
-        --dockerfile $CI_PROJECT_DIR/$DOCKER_FILE \
-        --destination $DOCKER_REGISTRY/$DOCKER_IMAGE_NAME:DEV-$VERSION
-  when: manual
-  environment:
-    name: development
-  only:
-    - develop
-
-uat:
-  stage: uat
-  dependencies:
-    - build
-  image:  $LZ_KANIKO_EXECUTOR_IMAGE
-  extends:
-    - .kaniko-login
-  script:
-    - echo "Deploying to uat environment"
-    - |
-      /kaniko/executor \
-        --context $CI_PROJECT_DIR \
-        --dockerfile $CI_PROJECT_DIR/$DOCKER_FILE \
-        --destination $DOCKER_REGISTRY/$DOCKER_IMAGE_NAME:RELEASE-$VERSION
-  when: manual
-  environment:
-    name: uat
-  only:
-    - release
-
-production:
-  stage: production
-  dependencies:
-    - build
-  image:  $LZ_KANIKO_EXECUTOR_IMAGE
-  extends:
-    - .kaniko-login
-  script:
-    - echo "Deploying to production environment"
-    - |
-      /kaniko/executor \
-        --context $CI_PROJECT_DIR \
-        --dockerfile $CI_PROJECT_DIR/$DOCKER_FILE \
-        --destination $DOCKER_REGISTRY/$DOCKER_IMAGE_NAME:$VERSION
-  when: manual
-  environment:
-    name: production
-  only:
-    - master
+---
+apiVersion: v1
+kind: Namespace
+metadata:
+  name: data-transfer
+---
+apiVersion: v1
+kind: ServiceAccount
+metadata:
+  name: s3-move
+  namespace: data-transfer
+  annotations:
+    eks.amazonaws.com/role-arn: arn:aws:iam::596599667803:role/role-0072-d-eks-lpbr-access-s3
+---
+apiVersion: batch/v1
+kind: Job
+metadata:
+  name: s3-move-once
+  namespace: data-transfer
+spec:
+  backoffLimit: 0
+  template:
+    spec:
+      serviceAccountName: s3-move
+      automountServiceAccountToken: true
+      restartPolicy: Never
+      containers:
+        - name: mover
+          image: public.ecr.aws/aws-cli/aws-cli:2.17.7
+          env:
+            - name: SRC_BUCKET
+              value: "s3-integration-a-847447826148-sa-east-1"
+            - name: SRC_PREFIX
+              value: "eCrlv/registros"        # pode ser vazio ""s3://s3-integration-test-d-596599667803-sa-east-1/eCrlv/registros/
+            - name: DST_BUCKET
+              value: "s3-integration-test-d-596599667803-sa-east-1"
+            - name: DST_PREFIX
+              value: "eCrlv/registros"         # pode ser vazio ""
+            - name: SRC_REGION
+              value: "sa-east-1"
+            - name: DST_REGION
+              value: "sa-east-1"
+            # # Defina estas se for KMS na prod:
+            # - name: DST_KMS_KEY_ID
+            #   value: "<PROD_KMS_KEY_ARN_OR_ID>"
+            # (opcional) filtros:
+            - name: EXTRA_FLAGS
+              value: "--exclude \"*.tmp\""
+          command: ["sh","-c"]
+          args:
+            - >
+              set -euo pipefail;
+              echo "Iniciando sync ${SRC_BUCKET}/${SRC_PREFIX} -> ${DST_BUCKET}/${DST_PREFIX}";
+              BASE_CMD="aws s3 sync s3://${SRC_BUCKET}/${SRC_PREFIX} s3://${DST_BUCKET}/${DST_PREFIX}
+              --only-show-errors --exact-timestamps --source-region ${SRC_REGION} --region ${DST_REGION}";
+              if [ -n "${DST_KMS_KEY_ID:-}" ]; then
+                BASE_CMD="$BASE_CMD --sse aws:kms --sse-kms-key-id ${DST_KMS_KEY_ID}";
+              fi;
+              if [ -n "${EXTRA_FLAGS:-}" ]; then
+                BASE_CMD="$BASE_CMD ${EXTRA_FLAGS}";
+              fi;
+              echo "Cmd: $BASE_CMD";
+              eval $BASE_CMD;
+              echo "Concluído.";
 
 
+#S3 resource
+module "aws_s3_integration" {
+  source = "git@gitlab.core-services.leaseplan.systems:shared/terraform_modules/aws/aws-s3-bucket.git?ref=v5.0.0"
+
+  name_prefix   = format("s3-integration-%s", module.shared_data.workload.environment_identifier)
+  versioning    = true
+  force_destroy = false
+
+  lifecycle_rule = [{
+    id     = "expire_old_versions"
+    status = "Enabled"
+    noncurrent_version_expiration = {
+      noncurrent_days = 10
+    }
+  }]
+  tags = local.tags
+
+}
+
+#S3 folders
+locals {
+  s3_folders = [
+    "eCrlv/registros/",
+    "eMultas/eventos/",
+    "eMultas/eventos/indicacao/",
+    "Workflow/tra/",
+    "TicketLog/Agreements/",
+    "TicketLog/Invoice/",
+  ]
+}
+
+resource "aws_s3_object" "s3_structure" {
+  for_each = toset(local.s3_folders)
+
+  bucket       = module.aws_s3_integration.bucket_id
+  key          = each.value
+  content_type = "application/x-directory"
+
+  override_provider {
+    default_tags {
+      tags = {}
+    }
+  }
+
+  depends_on = [
+    module.aws_s3_integration
+  ]
+}
+
+#IAM for S3
+data "aws_iam_policy_document" "policy_eks_lpbr_access_s3_assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+    effect  = "Allow"
+
+    condition {
+      test     = "StringEquals"
+      variable = "${replace(local.oidc_url, "https://", "")}:sub"
+      values = [
+        "system:serviceaccount:crlv:lpbr",
+        "system:serviceaccount:fines:lpbr-s3",
+        "system:serviceaccount:workflow:lpbr-s3",
+        "system:serviceaccount:glue-etl:lpbr-s3",
+        "system:serviceaccount:ticketlog:lpbr-s3"
+      ]
+    }
+
+    principals {
+      identifiers = [local.oidc_provider_arn]
+      type        = "Federated"
+    }
+  }
+}
+
+data "aws_iam_policy_document" "policy_eks_lpbr_access_s3" {
+  statement {
+    sid    = "ObjectAccess"
+    effect = "Allow"
+    actions = [
+      "s3:List*",
+      "s3:GetObject",
+      "s3:GetObjectAcl",
+      "s3:GetBucketAcl",
+      "s3:GetBucketLocation",
+      "s3:PutObject",
+      "s3:PutObjectAcl",
+      "s3:DeleteObjectVersion",
+      "s3:DeleteObject",
+    ]
+
+    resources = [
+      "arn:aws:s3:::s3-integration-*/*",
+      "arn:aws:s3:::s3-integration-*",
+      "arn:aws:s3:::s3-glue-etl-*/*",
+      "arn:aws:s3:::s3-glue-etl-*",
+    ]
+  }
+}
+// To do: remover after usage
+data "aws_iam_policy_document" "temp_cross_account_bucket_policy" {
+  statement {
+    sid    = "TempCrossAccountAccess"
+    effect = "Allow"
+    
+    principals {
+      type = "AWS"
+      identifiers = [
+        "arn:aws:iam::596599667803:role/role-0072-d-eks-lpbr-access-s3"
+      ]
+    }
+    
+    actions = [
+      "s3:List*",
+      "s3:GetObject",
+      "s3:GetObjectAcl",
+      "s3:GetBucketAcl",
+      "s3:GetBucketLocation",
+      "s3:PutObject",
+      "s3:PutObjectAcl",
+      "s3:DeleteObjectVersion",
+      "s3:DeleteObject",
+    ]
+    
+    resources = [
+      "arn:aws:s3:::s3-integration-a-847447826148-sa-east-1",
+      "arn:aws:s3:::s3-integration-a-847447826148-sa-east-1/*"
+    ]
+
+  }
+}
+
+// To do: remover after usage
+resource "aws_s3_bucket_policy" "temp_cross_account_access" {
+  bucket = "s3-integration-a-847447826148-sa-east-1"
+  policy = data.aws_iam_policy_document.temp_cross_account_bucket_policy.json
+}
+
+resource "aws_iam_policy" "policy_eks_lpbr_access_s3" {
+  name        = format("policy-0072-%s-eks-lpbr-access-s3", module.shared_data.workload.environment_identifier)
+  path        = "/"
+  description = "Allow lpbr app to access S3 buckets."
+  policy      = data.aws_iam_policy_document.policy_eks_lpbr_access_s3.json
+  tags        = local.tags
+}
+
+resource "aws_iam_role_policy_attachment" "role_eks_lpbr_access_s3" {
+  role       = aws_iam_role.role_eks_lpbr_access_s3.name
+  policy_arn = aws_iam_policy.policy_eks_lpbr_access_s3.arn
+}
+
+resource "aws_iam_role" "role_eks_lpbr_access_s3" {
+  name                 = format("role-0072-%s-eks-lpbr-access-s3", module.shared_data.workload.environment_identifier)
+  permissions_boundary = local.workload_boundary_arn
+  assume_role_policy   = data.aws_iam_policy_document.policy_eks_lpbr_access_s3_assume_role_policy.json
+  tags                 = local.tags
+}
 
 
-FROM leaseplan.jfrog.io/art-0072-docker-virtual/amazoncorretto:21 as builder
 
-ARG APPLICATION_PORT
-ARG CREDENTIALS_USER
-ARG CREDENTIALS_PASSWORD
-ARG DIRETORIO_PATH
-ARG ERROR_URI
-ARG EVENT_URI
-ARG AWS_WEB_IDENTITY_TOKEN_FILE
-ARG AWS_ROLE_ARN
-ARG AWS_ROLE_SESSION_NAME
-ENV APPLICATION_PORT=$APPLICATION_PORT
-ENV CREDENTIALS_USER=$CREDENTIALS_USER
-ENV CREDENTIALS_PASSWORD=$CREDENTIALS_PASSWORD
-ENV DIRETORIO_PATH=$DIRETORIO_PATH
-ENV ERROR_URI=$ERROR_URI
-ENV EVENT_URI=$EVENT_URI
-ENV AWS_WEB_IDENTITY_TOKEN_FILE=$AWS_WEB_IDENTITY_TOKEN_FILE
-ENV AWS_ROLE_ARN=$AWS_ROLE_ARN
-ENV AWS_ROLE_SESSION_NAME=$AWS_ROLE_SESSION_NAME
-ENV MAVEN_HOME /usr/share/maven
 
-COPY --from=maven:3.9.4-eclipse-temurin-11 ${MAVEN_HOME} ${MAVEN_HOME}
-COPY --from=maven:3.9.4-eclipse-temurin-11 /usr/local/bin/mvn-entrypoint.sh /usr/local/bin/mvn-entrypoint.sh
-COPY --from=maven:3.9.4-eclipse-temurin-11 /usr/share/maven/ref/settings-docker.xml /usr/share/maven/ref/settings-docker.xml
 
-RUN ln -s ${MAVEN_HOME}/bin/mvn /usr/bin/mvn
+#S3 resource
+module "aws_s3_integration" {
+  source = "git@gitlab.core-services.leaseplan.systems:shared/terraform_modules/aws/aws-s3-bucket.git?ref=v5.0.0"
 
-ARG MAVEN_VERSION=3.9.4
-ARG USER_HOME_DIR="/root"
-ENV MAVEN_CONFIG "$USER_HOME_DIR/.m2"
-ENTRYPOINT ["/usr/local/bin/mvn-entrypoint.sh"]
+  name_prefix   = format("s3-integration-%s", module.shared_data.workload.environment_identifier)
+  versioning    = true
+  force_destroy = false
 
-WORKDIR /usr/src/app
-COPY ./. .
+  lifecycle_rule = [{
+    id     = "expire_old_versions"
+    status = "Enabled"
+    noncurrent_version_expiration = {
+      noncurrent_days = 10
+    }
+  }]
+  tags = local.tags
 
-RUN yes | keytool -trustcacerts -keystore "/usr/lib/jvm/java-21-amazon-corretto/lib/security/cacerts" -storepass changeit -importcert -file "/usr/src/app/certificados/ZscalerIntermediateRootCA-zscloud.net.crt" -alias ZScaler
+}
 
-RUN yes | keytool -trustcacerts -keystore "/usr/lib/jvm/java-21-amazon-corretto/lib/security/cacerts" -storepass changeit -importcert -file "/usr/src/app/certificados/repo.maven.apache.org.crt" -alias RepoMaven
+#S3 folders
+locals {
+  s3_folders = [
+    "eCrlv/registros/",
+    "eMultas/eventos/",
+    "eMultas/eventos/indicacao/",
+    "Workflow/tra/",
+    "TicketLog/Agreements/",
+    "TicketLog/Invoice/",
+  ]
+}
 
-RUN mvn -f /usr/src/app/cadastro-positivo/pom.xml clean package
+resource "aws_s3_object" "s3_structure" {
+  for_each = toset(local.s3_folders)
 
-FROM leaseplan.jfrog.io/art-0072-docker-virtual/amazoncorretto:21-alpine-full
-COPY --from=builder /usr/src/app/certificados/ZscalerIntermediateRootCA-zscloud.net.crt /usr/share/ca-certificates/ZscalerIntermediateRootCA-zscloud.net.crt
-RUN yes | keytool -trustcacerts -keystore "/usr/lib/jvm/default-jvm/lib/security/cacerts" -storepass changeit -importcert -file "/usr/share/ca-certificates/ZscalerIntermediateRootCA-zscloud.net.crt" -alias ZScaler
-COPY --from=builder /usr/src/app/cadastro-positivo/cadastro-positivo-diretorio/target/cadastro-positivo-diretorio-1.0.0.jar /usr
-ENTRYPOINT ["java","-jar","/usr/cadastro-positivo-diretorio-1.0.0.jar"]
+  bucket       = module.aws_s3_integration.bucket_id
+  key          = each.value
+  content_type = "application/x-directory"
+
+  override_provider {
+    default_tags {
+      tags = {}
+    }
+  }
+
+  depends_on = [
+    module.aws_s3_integration
+  ]
+}
+// To do: delete after usage
+module "aws_s3_integration_test" {
+  source = "git@gitlab.core-services.leaseplan.systems:shared/terraform_modules/aws/aws-s3-bucket.git?ref=v5.0.0"
+
+  name_prefix   = format("s3-integration-test-%s", module.shared_data.workload.environment_identifier)
+
+  versioning    = true
+  force_destroy = true  
+
+  lifecycle_rule = [{
+    id     = "expire_old_versions"
+    status = "Enabled"
+    noncurrent_version_expiration = {
+      noncurrent_days = 10
+    }
+  }]
+
+  tags = local.tags
+}
+
+resource "aws_s3_object" "s3_structure_test" {
+  for_each = toset(local.s3_folders)
+
+  bucket       = module.aws_s3_integration_test.bucket_id
+  key          = each.value
+  content_type = "application/x-directory"
+  override_provider {
+    default_tags { tags = {} }
+  }
+
+  depends_on = [module.aws_s3_integration_test]
+}
+
+#IAM for S3
+data "aws_iam_policy_document" "policy_eks_lpbr_access_s3_assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+    effect  = "Allow"
+
+    condition {
+      test     = "StringEquals"
+      variable = "${replace(local.oidc_url, "https://", "")}:sub"
+      values = [
+        "system:serviceaccount:crlv:lpbr",
+        "system:serviceaccount:fines:lpbr-s3",
+        "system:serviceaccount:workflow:lpbr-s3",
+        "system:serviceaccount:glue-etl:lpbr-s3",
+        "system:serviceaccount:ticketlog:lpbr-s3",
+        // To do: remover after usage
+        "system:serviceaccount:data-transfer:s3-mover"
+      ]
+    }
+
+    principals {
+      identifiers = [local.oidc_provider_arn]
+      type        = "Federated"
+    }
+  }
+}
+
+data "aws_iam_policy_document" "policy_eks_lpbr_access_s3" {
+  statement {
+    sid    = "ObjectAccess"
+    effect = "Allow"
+    actions = [
+      "s3:List*",
+      "s3:GetObject",
+      "s3:GetObjectAcl",
+      "s3:GetBucketAcl",
+      "s3:GetBucketLocation",
+      "s3:PutObject",
+      "s3:PutObjectAcl",
+      "s3:DeleteObjectVersion",
+      "s3:DeleteObject",
+    ]
+
+    resources = [
+      "arn:aws:s3:::s3-integration-*/*",
+      "arn:aws:s3:::s3-integration-*",
+      "arn:aws:s3:::s3-glue-etl-*/*",
+      "arn:aws:s3:::s3-glue-etl-*",
+      // To do: remover after usage
+      "arn:aws:s3:::s3-integration-a-847447826148-sa-east-1",
+      "arn:aws:s3:::s3-integration-a-847447826148-sa-east-1/*",
+    ]
+  }
+}
+
+resource "aws_iam_policy" "policy_eks_lpbr_access_s3" {
+  name        = format("policy-0072-%s-eks-lpbr-access-s3", module.shared_data.workload.environment_identifier)
+  path        = "/"
+  description = "Allow lpbr app to access S3 buckets."
+  policy      = data.aws_iam_policy_document.policy_eks_lpbr_access_s3.json
+  tags        = local.tags
+}
+
+resource "aws_iam_role_policy_attachment" "role_eks_lpbr_access_s3" {
+  role       = aws_iam_role.role_eks_lpbr_access_s3.name
+  policy_arn = aws_iam_policy.policy_eks_lpbr_access_s3.arn
+}
+
+resource "aws_iam_role" "role_eks_lpbr_access_s3" {
+  name                 = format("role-0072-%s-eks-lpbr-access-s3", module.shared_data.workload.environment_identifier)
+  permissions_boundary = local.workload_boundary_arn
+  assume_role_policy   = data.aws_iam_policy_document.policy_eks_lpbr_access_s3_assume_role_policy.json
+  tags                 = local.tags
+}
